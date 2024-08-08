5e29c5e1a88d8e632c219d3cb8d1b3319d838127
sushmoyr <sushmoyr@gmail.com>
2024-08-08 18:28:52 +0600

    Initial Commit

---
+++ src/main/java/net/bdbulksms/greenwebbulksms/GreenwebBulkSmsApplication.java
@@ -0,0 +1,9 @@
+package net.bdbulksms.greenwebbulksms;
+
+public class GreenwebBulkSmsApplication {
+
+    public static void main(String[] args) {
+//        SpringApplication.run(GreenwebBulkSmsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/net/bdbulksms/greenwebbulksms/GreenwebBulkSmsApplicationTests.java
@@ -0,0 +1,13 @@
+package net.bdbulksms.greenwebbulksms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GreenwebBulkSmsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
